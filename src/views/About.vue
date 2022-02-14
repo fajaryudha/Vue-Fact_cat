3b57f7af4c31b0372bcc02bcceb5576eff12f741
Fajar Yudha Pramana <fajaryudha23@gmail.com>
2022-02-14 09:04:24 +0700

    Initial Commit

---
+++ src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <!-- Display the data with a structured table, element-ui is ready for use -->
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'vue-facts',
+  setup() {
+    const facts = ref([]);
+    const getFacts = async () => {
+      // Get data from backend
+    };
+    onMounted(() => {
+      getFacts();
+    });
+
+    return {
+      facts,
+    };
+  },
+};
+
+</script>
